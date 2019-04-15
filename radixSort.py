72c6c9da397c8c56722555d1e22e8dbe9022c5ca
Raymond <rayfvfree@outlook.com>
2019-04-15 18:43:45 +0800

    update

---
+++ radixSort.py
@@ -0,0 +1,27 @@
+'''
+可以考慮用Linked list代替bucketList，Big-o會從O(K*N) 變成 O(N)
+'''
+
+import math
+import readFile as rf
+
+def radixSort(dataList, radix=10):
+    K = math.ceil(math.log(max(dataList)+1, radix))
+    print(K)
+    for i in range(1, K+1):
+        print(i)
+        bucketList = [[] for _ in range(radix)]
+
+        for data in dataList:
+            index = data%(radix**i)//(radix**(i-1))
+            bucketList[index].append(data)
+        
+        dataList.clear()
+
+        for bucket in bucketList:
+            dataList += bucket
+    
+test = rf.getNumberDataList()
+
+radixSort(test)
+print(test)
